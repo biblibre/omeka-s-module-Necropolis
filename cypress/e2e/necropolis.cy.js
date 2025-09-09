4905068e7b5832ace5b68759bf0449e4a3d8362c
abelb <abel.boilevin@biblibre.com>
2025-09-09 16:06:23 +0200

    Fix chosenOptions error 2.

--- cypress/e2e/necropolis.cy.js
+++ cypress/e2e/necropolis.cy.js
@@ -1,4 +1,13 @@
 it('Necropolis', function() {
+     Cypress.on('uncaught:exception', (err) => {
+     // omekas/install has a weird exception that will fail the test
+     // Ignore the specific ReferenceError we don't care about
+     if (err.message.includes('chosenOptions is not defined')) {
+          return false
+     }
+     // let all other errors fail the test
+     });
+
      cy.visit('http://omekas/install')
      cy.get('[name="user[email]"]').click();
      cy.get('[name="user[email]"]').clear();
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -26,11 +26,3 @@ beforeEach(() => {
    */
   cy.intercept('**/fonts/FiraCode-VF.woff2', {});
 });
-
-Cypress.on('uncaught:exception', (err) => {
-  // Ignore the specific ReferenceError we don't care about
-  if (err.message.includes('chosenOptions is not defined')) {
-    return false
-  }
-  // let all other errors fail the test
-})
